dd44f99afa4292e3d55733cc7c56e8cb526180a1
DragonRF <duonglong410@gmail.com>
2022-12-14 11:43:41 +0700

    first commit

---
+++ Web-HTTP/ThucHanh/Basic-Login/app.js
@@ -0,0 +1,10 @@
+const http = require('http')
+http.createServer(function (req, res){
+    let txt = ''
+    if (req.url === '/login'){
+        txt = 'Login Success'
+    } else {
+        txt = 'Login Fail'
+    }
+    res.end(txt)
+}).listen(8080,'localhost')
